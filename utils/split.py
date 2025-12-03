4f3f1991ce37a55dbc4aa35812cd80fbe4e6c7a4
Victor LUQUE <victor.luque75@gmail.com>
2025-12-03 09:37:00 +0100

    Checkpoint

--- utils/split.py
+++ utils/split.py
@@ -9,9 +9,9 @@ def split_data(df) :
     df.set_index(['date'],inplace = True)
 
     #Définition of the numbers of days for the train limit
-    date_debut = df.index.min()
+    date_fin = df.index.max()
     nb_jours = pd.Timedelta(days=28)
-    date_seuil = date_debut + nb_jours
+    date_seuil = date_fin - nb_jours
 
     #Création des Df
     df_train = df.loc[:date_seuil]
