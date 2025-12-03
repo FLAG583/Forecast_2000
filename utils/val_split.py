60649570ffc8c288d3d455895f45a265f61125e7
Victor LUQUE <victor.luque75@gmail.com>
2025-12-03 11:58:18 +0100

    Changement des variables de la function val_split

--- utils/val_split.py
+++ utils/val_split.py
@@ -1,15 +1,15 @@
-def val_split(df) :
+def val_split(X_train_prepo ,y_train) :
     #Définition du nombre de jours pour le val set
     val_horizon = 28
     # Nombre total d'observations dans le train
-    n = len(X_train)
+    n = len(X_train_prepo)
     # Position où commence la validation
     val_start = n - val_horizon
     # Définition du val set
-    X_val = X_train.iloc[val_start : n]
+    X_val = X_train_prepo.iloc[val_start : n]
     y_val = y_train.iloc[val_start : n]
     # train final = du début du train au début du val start exclus
-    X_train = X_train.iloc[0 : val_start]
+    X_train = X_train_prepo.iloc[0 : val_start]
     y_train= y_train.iloc[0 : val_start]
 
     return X_train,y_train,X_val, y_val
