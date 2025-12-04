f20561fa3d0bdbaa3d20993c7ac05dd069de96e8
Bertrand Lesne <enselb@gmail.com>
2025-12-04 11:15:38 +0100

    correctif modele lightGBM & ajout à requirement.txt

--- forecast_2000/models/model_LightGBM.py
+++ forecast_2000/models/model_LightGBM.py
@@ -1,15 +1,16 @@
 import lightgbm as lgb
 from sklearn.metrics import mean_squared_error
 import numpy as np
+from typing import Tuple
 
 
-def model_LightGBM(X_train, y_train, X_val, y_val, X_test, y_test):
+def train_model_LightGBM(X_train, y_train, X_val, y_val, X_test, y_test):
     # Convert to Pandas category
-    X = [X_train, X_val, X_test]
-    for df in X:
-        cats = df.select_dtypes(exclude=np.number).columns.tolist()
-        for col in cats:
-            df[col] = df[col].astype('category')
+    # X = [X_train, X_val, X_test]
+    # for df in X:
+    #     cats = df.select_dtypes(exclude=np.number).columns.tolist()
+    #     for col in cats:
+    #         df[col] = df[col].astype('category')
 
     # identification des colonnes catégorielles car LightGBM gère mieux les catégories que le One-Hot Encoding
     categorical_cols = ['id', 'item_id', 'dept_id', 'cat_id', 'store_id', 'state_id',
@@ -32,7 +33,7 @@ def model_LightGBM(X_train, y_train, X_val, y_val, X_test, y_test):
             lgb.early_stopping(stopping_rounds=50),
             lgb.log_evaluation(period=100)
         ]
-
+    print("Entraînement du modèle LightGBM...")
     lightGBM_model.fit(
             X_train,
             y_train,
@@ -42,11 +43,18 @@ def model_LightGBM(X_train, y_train, X_val, y_val, X_test, y_test):
             callbacks=callbacks
         )
 
+    return lightGBM_model
+
+def evaluate_and_predict(model: lgb.LGBMRegressor, X_test, y_test) -> Tuple[lgb.LGBMRegressor, np.ndarray]:
+    """
+    Évalue le modèle sur le jeu de test et retourne les prédictions.
+    """
     # Prédictions
-    y_pred = lightGBM_model.predict(X_test)
+    print("Génération des prédictions sur le jeu de test...")
+    y_pred = model.predict(X_test)
 
     # Performance
-    rmse = mean_squared_error(y_test, y_pred)
-    print(f"RMSE sur le test set : {rmse:.4f}")
+    rmse = np.sqrt(mean_squared_error(y_test, y_pred))
+    print(f"✅ RMSE sur le jeu de test : {rmse:.4f}")
 
-    return lightGBM_model, y_pred
+    return y_pred
