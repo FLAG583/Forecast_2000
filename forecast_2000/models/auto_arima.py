f1c81b6ba4d9e977b6320e9387aa280dffc665ec
Deborah Caruge <debocaruge@gmail.com>
2025-12-09 14:16:32 +0100

    return fonction uptaded

--- forecast_2000/models/auto_arima.py
+++ forecast_2000/models/auto_arima.py
@@ -4,11 +4,12 @@ def auto_arima(y_train):
 
     # Construction et entrainement du modèle auto ARIMA
 
-    smodel = pm.auto_arima(y_train,
+    model = pm.auto_arima(y_train,
                         start_p=0, max_p=3,
                         start_q=0, max_q=0,
                         trend='t',
                         seasonal=False,
                         trace=True)
 
-    return smodel
+    y_pred = model.predict()
+    return y_pred, model
